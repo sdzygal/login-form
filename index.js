43c866a6fdf6887edfe35d5ebc8cc5aaa7bfcf4b
szdygal <sdzygal@gmail.com>
2022-11-05 13:46:21 -0600

    Selected switch

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const signUp = document.getElementById('register');
 const signIn = document.getElementById('login');
 
+
 signUp.addEventListener('click', () => {
     const form = document.getElementById('signUp');
     document.getElementById('signIn').style.display = 'none';
+    signUp.className = 'selected';
 
     if(form.style.display === 'none') {
         form.style.display = 'block';
     }
+    
 });
+
 
 signIn.addEventListener('click', () => {
     const form = document.getElementById('signIn');
     document.getElementById('signUp').style.display = 'none';
+    signIn.className = 'selected';
 
     if(form.style.display === 'none') {
         form.style.display = 'block';
     }
+    
 });
+
+function selectElement() {
+    if(signIn === 'selected') {
+        signUp.remove = 'selected';
+    } else if(signUp === 'selected') {
+        signIn.remove = 'selected';
+    }
+};
 
 function validate() {
     let username = document.getElementById("username").value;
